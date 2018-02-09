120e70bee3a7010062a2e1dbd613fb5fadc539ba
Petr Petukhov <kotov0991@gmail.com>
2018-02-09 23:32:14 +0300

    768

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,6 @@
 
 $(function() {
 
-	// Выезжающий блок вверху страницы
-	$(document).ready(function(){
-		$(".toggle-btn").click(function(){
-			$('.toggle-btn').toggleClass("on");
-			$(".top-line__content").slideToggle("200");
-		});
-	});
-
-
-
 	// Фиксирование состояние рекомендованной карточки тарифного плана
 	$(".table-pricing").hover( function(){
 		$(".table-pricing").eq(0).removeClass('recomend');	
